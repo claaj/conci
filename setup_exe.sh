54a0d9073e4fdab5347b06f4890794e93d129515
claaj <matias.cajal@hotmail.com>
2023-08-26 14:26:21 -0300

    Setup para packaging + icon

--- setup_exe.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/sh
-
-poetry run pyinstaller -F -n conci -w --collect-all tk --collect-all customtkinter --collect-all pillow --collect-all CTkMessagebox --hidden-import='PIL._tkinter_finder' src/main.py
